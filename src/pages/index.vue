052fea77418bf527fc45c3789ba37f41ca3d3b79
K1nGsmaN-hub <15162342h@gmail.com>
2022-10-21 21:33:18 +0800

    feat(pages): add Characters page

---
+++ src/pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <Characters :characters="characters" />
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import CharacterCard from 'molecules/CharacterCard/index.vue'
+import RFInput from 'atoms/RFInput/index.vue'
+import { ICharacters } from 'types/DTO/Characters'
+import Characters from "templates/Characters/index.vue";
+
+export default Vue.extend({
+  name: 'Index',
+  components: { Characters, RFInput, CharacterCard },
+  async asyncData({ store }) {
+    await store.dispatch('characters/fetchCharacters')
+  },
+  computed: {
+    characters(): ICharacters[] {
+      return this.$store.getters['characters/getCharacters']
+    },
+  },
+})
+</script>
