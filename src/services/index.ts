dbcc34df95164766f1cdc4f9070f84e06ebbb263
K1nGsmaN-hub <15162342h@gmail.com>
2022-10-21 21:31:55 +0800

    feat(services): remove CommentSerialization

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,6 +1,5 @@
 import { Context } from '@nuxt/types'
 import { Image } from 'services/common/Image'
-import CommentSerialization from './CommentSerialization/index'
 import FieldValidator, { IFieldValidator } from './FieldValidator/index'
 import ErrorHandler, { IErrorHandler } from './common/ErrorHandler'
 import HttpClient, { IHttpClient } from './common/HttpClient'
@@ -13,7 +12,6 @@ export type servicesType = {
   common: commonServices
   image: Image
   fieldValidator: IFieldValidator
-  commentSerialization: CommentSerialization
 }
 
 const services = (ctx: Context): servicesType => ({
@@ -23,7 +21,6 @@ const services = (ctx: Context): servicesType => ({
   },
   image: new Image(),
   fieldValidator: new FieldValidator(),
-  commentSerialization: new CommentSerialization(),
 })
 
 export default services
